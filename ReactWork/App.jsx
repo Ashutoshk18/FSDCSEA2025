44a4987ca0646c08402aa1d4d6c0533fd318d470
Ashutoshk18 <ashutoshku1803@gmail.com>
2025-10-14 15:31:00 +0530

    14 Oct 2025

---
+++ ReactWork/App.jsx
@@ -0,0 +1,39 @@
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+const h2 = React.createElement(
+  "h2",
+  { style: { color: "brown", fontSize: "larger", textAlign: "center" } },
+  "ABES Engineering College"
+);
+const li1 = React.createElement("li", {}, "React");
+const li2 = React.createElement("li", {}, "Java");
+const li3 = React.createElement("li", {}, "Python");
+const Name = React.createElement("h2", {}, "Ashutosh Kumar");
+const ul = React.createElement(
+  "ul",
+  {
+    sytle: { backgroundColor: "black", color: "white" },
+  },
+  li1,
+  li2,
+  li3
+);
+const img = React.createElement("img", {
+  src: "https://picsum.photos/536/354",
+  style: { borderRadius: "50%", backgroundColor: "blue", width: "200px" },
+});
+const nameDiv = React.createElement(
+  "div",
+  { style: { display: "flex" } },
+  Name,
+  ul
+);
+const nameImg = React.createElement(
+  "div",
+  {
+    style: { display: "flex", justifyContent: "space-evenly", width: "100vh" },
+  },
+  img,
+  nameDiv
+);
+const wrapper = React.createElement("div", {}, h2, nameImg);
+root.render(wrapper);
