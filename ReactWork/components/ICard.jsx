029cf8b298e52f55832b4b97747ece7f21590f7d
Ashutoshk18 <ashutoshku1803@gmail.com>
2025-10-16 10:27:13 +0530

    React-Props

---
+++ ReactWork/components/ICard.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+function ICard(props) {
+  console.log(props);
+  return <h1>afldk</h1>;
+}
+export default ICard();
---
+++ ReactWork/components/Profile.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Profile() {
+  console.log("What is happening?");
+  return <h1>Hello World! This a new component of Profile</h1>;
+}
+
+export default Profile();
